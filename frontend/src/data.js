aa62597a1ad01697667a0435705aa89e7e55ad37
gabriel favour <favvyghabs@gmail.com>
2022-02-07 13:28:15 +0100

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,67 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'sport car',
+            category: 'cars',
+            image: '/images/p4.jpg',
+            price: 120000,
+            brand: 'sport cars',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality cars'
+        },
+        {
+            _id: '2',
+            name: 'Bugatti verizone',
+            category: 'cars',
+            image: '/images/p7.jpg',
+            price: 120000000,
+            brand: 'sport cars',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality cars'
+        }, {
+            _id: '3',
+            name: 'Mercedes benz',
+            category: 'cars',
+            image: '/images/p2.jpg',
+            price: 150000,
+            brand: 'Mercedes',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality cars'
+        }, {
+            _id: '4',
+            name: 'Lamborghini',
+            category: 'cars',
+            image: '/images/p5.jpg',
+            price: 500000,
+            brand: 'Lamborghini',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality cars'
+        }, {
+            _id: '5',
+            name: 'Mercedes Benz',
+            category: 'cars',
+            image: '/images/p1.jpg',
+            price: 100000,
+            brand: 'Mercedes',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality cars'
+        }, {
+            _id: '6',
+            name: 'super car',
+            category: 'cars',
+            image: '/images/p6.jpg',
+            price: 3000000000,
+            brand: 'super cars',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality cars'
+        },
+    ],
+};
+export default data;
